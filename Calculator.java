786c875ca58774180cecfc09b9c0ef331e8852d2
Vamos_Baek <ibaekhj@yahoo.com>
2016-05-16 11:13:45 +0900

    create Calculator

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+  public static void main(String[] args) {
+    System.out.println("Calculator class started");
+  Calculator calc=new Calculator();
+  int a=10;
+  int b=5;
+  calc.add(a,b);
+  calc.subtract(a,b);
+  calc.multiply(a,b);
+  clac.divide(a,b);
+  }
+}
